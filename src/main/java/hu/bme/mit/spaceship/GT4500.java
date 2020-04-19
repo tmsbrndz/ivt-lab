56e4d3d11c933cdeca0dcde1562d61af8b6bdb1e
tmsbrndz <31684353+tmsbrndz@users.noreply.github.com>
2020-04-19 17:45:49 +0200

    comment modified

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        //TODO implement feature.....
         
       int primaryTorpedoStoreCount = primaryTorpedoStore.getTorpedoCount();
       int secondaryTorpedoStoreCount = secondaryTorpedoStore.getTorpedoCount();
